bf21c59a6453687c41823ea31a2a7e68e9fdc112
calvin <421270473@qq.com>
2018-09-02 17:09:13 +0800

    更新，新增暴露的方法

--- src/wrap-text.js
+++ src/wrap-text.js
@@ -1,6 +1,42 @@
 'use strict'
 
 export default function extendCanvas () {
+  CanvasRenderingContext2D.prototype.toLines = function (text, width) {
+    let words = text.split('')
+    let wordsLen = words.length
+    let line = ''
+    let lines = []
+    // 匹配这些中文标点符号，中文标点不能作为行首
+    let symbolReg = /[\。\？\！\，\、\；\：\“\”\‘\'\（\）\《\》\〈\〉\【\】\『\』\「\」\﹃\﹄\〔\〕\…\—\～\﹏\￥]/
+    let englishReg = /[a-zA-Z]+/
+
+    function shouldMergeWord (nowWord, nextWord) {
+      return symbolReg.test(nextWord) || (englishReg.test(nowWord) && englishReg.test(nextWord))
+    }
+
+    for (let i = 0; i < wordsLen;) {
+      let word = words[i]
+      // 按照word-break的break-word原则合并word
+      if (i + 1 < wordsLen && shouldMergeWord(word, words[i + 1])) {
+        words[i + 1] = word + words[i + 1]
+        i++
+        continue
+      }
+      // 检测是否需要换行
+      let tempLine = line + word
+      if (this.measureText(tempLine).width > width) {
+        lines.push(line)
+        line = ''
+        continue
+      }
+      line = tempLine
+      if (i + 1 === wordsLen) {
+        lines.push(line)
+      }
+      i++
+    }
+    return lines
+  }
   CanvasRenderingContext2D.prototype.wrapText = function (text, x, y, width, lineHeight) {
     if (typeof text !== 'string') throw new Error('text not string')
 
@@ -8,43 +44,6 @@ export default function extendCanvas () {
       if (Number.isNaN(Number(defaultVal))) throw new Error('defaultVal type must be number')
       let valNum = Number(val)
       return Number.isNaN(valNum) ? defaultVal : valNum
-    }
-
-    function toLines (text) {
-      let words = text.split('')
-      let wordsLen = words.length
-      let line = ''
-      let lines = []
-      // 匹配这些中文标点符号，中文标点不能作为行首
-      let symbolReg = /[\。\？\！\，\、\；\：\“\”\‘\'\（\）\《\》\〈\〉\【\】\『\』\「\」\﹃\﹄\〔\〕\…\—\～\﹏\￥]/
-      let englishReg = /[a-zA-Z]+/
-
-      function shouldMergeWord (nowWord, nextWord) {
-        return symbolReg.test(nextWord) || (englishReg.test(nowWord) && englishReg.test(nextWord))
-      }
-
-      for (let i = 0; i < wordsLen;) {
-        let word = words[i]
-        // 按照word-break的break-word原则合并word
-        if (i + 1 < wordsLen && shouldMergeWord(word, words[i + 1])) {
-          words[i + 1] = word + words[i + 1]
-          i++
-          continue
-        }
-        // 检测是否需要换行
-        let tempLine = line + word
-        if (this.measureText(tempLine).width > width) {
-          lines.push(line)
-          line = ''
-          continue
-        }
-        line = tempLine
-        if (i + 1 === wordsLen) {
-          lines.push(line)
-        }
-        i++
-      }
-      return lines
     }
 
     let canvas = this.canvas
@@ -55,7 +54,7 @@ export default function extendCanvas () {
     y = toNumber(y)
     width = toNumber(width, canvas.width)
     lineHeight = toNumber(lineHeight, defaultLineHeight)
-    let lines = toLines.call(this, text)
+    let lines = this.toLines.call(this, text, width)
     lines.forEach((line, index) => {
       this.fillText(line, x, y + lineHeight * index)
     })
